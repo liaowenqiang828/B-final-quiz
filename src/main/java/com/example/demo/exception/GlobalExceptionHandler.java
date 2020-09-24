37c541ad6c05004a1889837a37ba995edc818762
liaowenqiang828 <18840965351@163.com>
2020-09-24 14:52:41 +0800

    [Wenqiang Liao] feat: add exception handler for trainer not exists

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo.exception;
 
+import com.example.demo.constants.ErrorConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,7 +15,17 @@ public class GlobalExceptionHandler {
         String message = exception.getMessage();
         ErrorResult errorResult = ErrorResult.builder()
                 .message(message)
-                .detail(new HashMap<String, String>(){{put("exists", "trainee is not exists");}})
+                .detail(new HashMap<String, String>(){{put("exists", ErrorConstants.TRAINEE_NOT_EXISTS);}})
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
+    }
+
+    @ExceptionHandler(TrainerNotExists.class)
+    public ResponseEntity<ErrorResult> handler(TrainerNotExists exception) {
+        String message = exception.getMessage();
+        ErrorResult errorResult = ErrorResult.builder()
+                .message(message)
+                .detail(new HashMap<String, String>(){{put("exists", ErrorConstants.TRAINER_NOT_EXISTS);}})
                 .build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
     }
