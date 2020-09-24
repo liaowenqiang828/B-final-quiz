14a213f98145fac19ebc3baed5af05f4e6b18fa1
liaowenqiang828 <18840965351@163.com>
2020-09-24 14:29:39 +0800

    [Wenqiang Liao] feat: add error message constants

--- src/main/java/com/example/demo/constants/ErrorConstants.java
+++ src/main/java/com/example/demo/constants/ErrorConstants.java
@@ -2,4 +2,10 @@ package com.example.demo.constants;
 
 public class ErrorConstants {
     public static final String TRAINEE_NOT_EXISTS = "trainee not exists";
+    public static final String ZOOM_ID_ERROR_MESSAGE = "Zoom ID is mandatory";
+    public static final String OFFICE_ERROR_MESSAGE = "office is mandatory";
+    public static final String EMAIL_ERROR_MESSAGE = "email is mandatory";
+    public static final String NAME_ERROR_MESSAGE = "name is mandatory";
+    public static final String EMAIL_FORMAT_ERROR_MESSAGE = "email format wrong";
+    public static final String GITHUB_ERROR_MESSAGE = "github is mandatory";
 }
--- src/main/java/com/example/demo/domain/Trainee.java
+++ src/main/java/com/example/demo/domain/Trainee.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain;
 
+import com.example.demo.constants.ErrorConstants;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,16 +21,16 @@ public class Trainee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull
+    @NotNull(message = ErrorConstants.NAME_ERROR_MESSAGE)
     private String name;
-    @NotNull
+    @NotNull(message = ErrorConstants.OFFICE_ERROR_MESSAGE)
     private String office;
-    @NotNull
-    @Email
+    @NotNull(message = ErrorConstants.EMAIL_ERROR_MESSAGE)
+    @Email(message = ErrorConstants.EMAIL_FORMAT_ERROR_MESSAGE)
     private String email;
-    @NotNull
+    @NotNull(message = ErrorConstants.ZOOM_ID_ERROR_MESSAGE)
     private String zoomId;
-    @NotNull
+    @NotNull(message = ErrorConstants.GITHUB_ERROR_MESSAGE)
     private String gitHub;
 
     @JsonIgnore
